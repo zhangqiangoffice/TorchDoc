3abc7f5f9db0bfd41a0e5ddc36b813fa7ad64b20
zhangqiangoffice <zhangqiangoffice@163.com>
2016-05-03 21:52:02 +0800

    实现动态页面

--- meadowlark.js
+++ meadowlark.js
@@ -1,29 +1,35 @@
 var express = require('express');
 var app = express();
 
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', j=handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
-	res.type('text/plain');
-	res.send('Meadowlark Travel');
+	res.render('home');
 });
 
+var fortunes = ["Conquer", "Rivers", "Fears", "Surprise", "Simple"];
 app.get('/about', function(req, res){
-	res.type('text/plain');
-	res.send('About Meadowlark Travel');
+	var randomFortune = 
+			fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', {fortune: randomFortune});
 });
 
 app.use(function(req, res) {
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
 
 app.use(function(err, req, res, next){
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
 app.listen(app.get('port'), function(){
